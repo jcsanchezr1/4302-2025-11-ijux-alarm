e854c447ebb5deaaf2e7bd39d99d825eef88aaf6
jcsanchezr1 <jc.sanchezr1@uniandes.edu.co>
2025-03-12 01:48:59 -0500

    Se ajusta pantalla de listado de alarmas acorde al Figma

--- mobile/app/src/main/java/com/example/api/ijux/alarm/ListAlarmActivity.kt
+++ mobile/app/src/main/java/com/example/api/ijux/alarm/ListAlarmActivity.kt
@@ -1,10 +1,12 @@
 package com.example.api.ijux.alarm
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatImageButton
 
@@ -23,6 +25,8 @@ class ListAlarmActivity : AppCompatActivity() {
 
         val layoutListAlarms = findViewById<LinearLayout>(R.id.LayoutListAlarms)
         val layoutTextNoAlarms = findViewById<LinearLayout>(R.id.LayoutTextNoAlarms)
+        val textViewNoAlarms1 = findViewById<TextView>(R.id.textViewNoAlarms1)
+        val textViewNoAlarms2 = findViewById<TextView>(R.id.textViewNoAlarms2)
 
         if (hasAlarms) {
             layoutListAlarms.visibility = View.VISIBLE
@@ -30,6 +34,8 @@ class ListAlarmActivity : AppCompatActivity() {
         } else {
             layoutListAlarms.visibility = View.GONE
             layoutTextNoAlarms.visibility = View.VISIBLE
+            textViewNoAlarms1.setTextColor(Color.parseColor("#4F2380"))
+            textViewNoAlarms2.setTextColor(Color.parseColor("#4F2380"))
         }
     }
 
