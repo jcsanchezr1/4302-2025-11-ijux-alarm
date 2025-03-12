f7e1e800d001940a4ce69ee96ab23e6f6457b751
jcsanchezr1 <jc.sanchezr1@uniandes.edu.co>
2025-03-11 21:51:53 -0500

    Primera version de pantalla de listar alarmas

--- mobile/app/src/main/java/com/example/api/ijux/alarm/ListAlarmActivity.kt
+++ mobile/app/src/main/java/com/example/api/ijux/alarm/ListAlarmActivity.kt
@@ -1,11 +1,28 @@
 package com.example.api.ijux.alarm
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageButton
 
 class ListAlarmActivity : AppCompatActivity() {
+
+    private lateinit var buttonLogOut: AppCompatImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_alarm)
+        initViews()
+        setupLoginButton()
+    }
+
+    private fun initViews() {
+        buttonLogOut = findViewById(R.id.imageButtonLogOut)
+    }
+
+    private fun setupLoginButton() {
+        buttonLogOut.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
